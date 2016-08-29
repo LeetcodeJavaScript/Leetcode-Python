5ffc7593e3decec1e981d768a5769cb0975a9e86
LeetcodeJavaScript <LeetcodeJavaScript@outlook.com>
2016-08-28 17:51:38 -0700

    Create 2. add-two-numbers.py

---
+++ 2. add-two-numbers.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        c = 0
+        curr = before = ListNode(None)
+        
+        while l1 or l2 or c:
+            v1 = l1.val if l1 else 0
+            v2 = l2.val if l2 else 0
+            c += v1 + v2
+            
+            curr.next = ListNode(c % 10)
+            c = c // 10;
+            curr = curr.next
+            
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+            
+        return before.next
+        
